302794935811627a290421694775849ba2ce8779
Gaurav Tak <gauravtak996@gmail.com>
2022-12-24 15:26:45 +0530

    ejs engine added

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/', function (req,res) {
-    res.send("hello world");
+app.set('view engine', "ejs");
+
+app.get('/', (req,res)=>{
+    res.render("home");
 })
+
+app.get('/SignUp', (req,res)=>{
+    res.render('SignUp');
+})
+
+app.get('/SignIn', (req,res)=>{
+    res.render('SignIn');
+});
+
+
 
 app.listen(3000, ()=>{
     console.log('server listening at port 3000');
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
