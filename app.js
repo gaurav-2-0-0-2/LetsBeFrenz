aa6f84b845f21ab4c46796bc21a09789751a8823
Gaurav Tak <gauravtak996@gmail.com>
2022-12-24 14:08:03 +0530

    converted to node app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req,res) {
+    res.send("hello world");
+})
+
+app.listen(3000, ()=>{
+    console.log('server listening at port 3000');
+})
