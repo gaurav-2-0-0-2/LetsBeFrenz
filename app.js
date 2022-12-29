47a9e1bd8a2896154bab2f77235f2090f6d35f26
Gaurav Tak <gauravtak996@gmail.com>
2022-12-29 11:14:45 +0530

    database added

--- app.js
+++ app.js
@@ -13,11 +13,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.set('view engine', "ejs");
 
+mongoose.set('strictQuery', false);
 
 mongoose.connect("mongodb://localhost:27017/friendDB",{useNewUrlParser: true});
 
 
 
+// Create Schema
+const friendSchema = new mongoose.Schema({
+    email: String,
+    password: Array,
+});
+
+// Create Model through Schema
+const Friend = new mongoose.model("Friend", friendSchema);
 
 app.get('/', (req,res)=>{
     res.render("home");
@@ -40,16 +49,36 @@ app.get('/welcome', (req,res)=>{
 
 app.post('/SignUp', (req,res)=>{
     // console.log(req.body.username);
-    // console.log(req.body.password);
+    // console.log(req.body.password); 
+   const  newFriend = new Friend({
+    email: req.body.username,
+    password: req.body.password,
+    confirmPassword: req.body.password
+   });
 
-    res.render("welcome");
+   newFriend.save((err)=>{
+    if (err) {
+        console.log(err);
+    } else {
+        res.render("welcome");
+    }
+   })
+
+
+    
 })
 app.post('/SignIn', (req,res)=>{
     // console.log(req.body.username);
     // console.log(req.body.password);
+    Friend.findOne({
+        email: req.body.username,
+    });
+    if (req.body.password==Friend.password) {
+        res.render("welcome");
+    } else {
+        console.log("Oops! Wrong password");
+    }
     
-    res.render("welcome");
-   
 })
 
 
