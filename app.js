abacc036f93eaad1cba5f7a54eb283044f144b97
Gaurav Tak <gauravtak996@gmail.com>
2022-12-29 15:23:56 +0530

    you can sign up now

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', "ejs");
 
 mongoose.set('strictQuery', false);
 
-mongoose.connect("mongodb://localhost:27017/friendDB",{useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/friendDB", { useNewUrlParser: true });
 
 
 
@@ -28,61 +28,89 @@ const friendSchema = new mongoose.Schema({
 // Create Model through Schema
 const Friend = new mongoose.model("Friend", friendSchema);
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.render("home");
 })
 
-app.get('/SignUp', (req,res)=>{
+app.get('/SignUp', (req, res) => {
     res.render('SignUp');
 })
 
-app.get('/SignIn', (req,res)=>{
+app.get('/SignIn', (req, res) => {
     res.render('SignIn');
 });
 
-app.get('/welcome', (req,res)=>{
-   res.render('welcome');
+app.get('/welcome', (req, res) => {
+    res.render('welcome');
 })
 
+// app.get("/signInFail", (req,res)=>{
+//     res.render("signInFail");
+// });
 
 
 
-app.post('/SignUp', (req,res)=>{
+
+app.post('/SignUp', (req, res) => {
     // console.log(req.body.username);
     // console.log(req.body.password); 
-   const  newFriend = new Friend({
-    email: req.body.username,
-    password: req.body.password,
-    confirmPassword: req.body.password
-   });
-
-   newFriend.save((err)=>{
-    if (err) {
-        console.log(err);
-    } else {
-        res.render("welcome");
-    }
-   })
-
-
-    
-})
-app.post('/SignIn', (req,res)=>{
-    // console.log(req.body.username);
-    // console.log(req.body.password);
-    Friend.findOne({
+    const newFriend = new Friend({
         email: req.body.username,
+        password: req.body.password,
+        confirmPassword: req.body.password
     });
-    if (req.body.password==Friend.password) {
-        res.render("welcome");
-    } else {
-        console.log("Oops! Wrong password");
-    }
-    
-})
+
+    newFriend.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("welcome");
+        }
+    })
+
+});
 
 
-app.listen(3000, ()=>{
+
+app.post('/SignIn', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    Friend.findOne({ email: username }, function (err, foundFriend) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundFriend) {
+                if (foundFriend.password === password) {
+                    res.render("welcome");
+                 }else{
+                    console.log("You've entered wrong password.")
+                 }
+            }
+        }
+    });
+
+
+});
+
+// app.post("/SignIn", (req,res)=>{
+//    const username = req.body.username;
+//    const password = req.body.password;
+
+//    Friend.findOne({email:username}, (err, foundFriend)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         if(foundFriend){
+//             if(foundFriend.password === password){
+//                 res.render("welcome");
+//             }
+//         }
+//     }
+//    });
+// });
+
+
+app.listen(3000, () => {
     console.log('server listening at port 3000');
 })
 
